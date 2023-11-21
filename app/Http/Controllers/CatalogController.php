0585d2880004393e8a9b35f1c65aab7cf913a2e3
DmitriKuharchuk <dmitri.kukharchuk@gmail.com>
2023-11-21 18:58:42 +0300

    up

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -75,11 +75,17 @@ class CatalogController
         Gate::authorize("catalog-edit", $record);
         $form = new CatalogAdminForm($record);
         $data = request()->all();
+        $change = false;
         if (isset($request->is_active)){
             $data['is_active'] = $request->is_active;
         }else{
             $data['is_active'] = $record->is_active;
         }
+
+        if ($data['is_active'] != $record->is_active){
+            $change = true;
+        }
+
         $form->formSave($data);
 
         $record = Catalog::query()
@@ -111,11 +117,12 @@ class CatalogController
             }
         }
 
-        Catalog::query()->whereIn('id',$array)
-            ->update([
-                'is_active' =>  $data['is_active']
-            ]);
-
+        if ($change == true){
+            Catalog::query()->whereIn('id',$array)
+                ->update([
+                    'is_active' =>  $data['is_active']
+                ]);
+        }
 
         return ["redirect" => route("catalog.list", 0)];
     }
